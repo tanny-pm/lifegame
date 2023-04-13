999ea543d6f76e285e4b7315fba2a1cc4cee16b4
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-04-13 22:04:36 +0900

    feat: Implement Conway's Game of Life with React, MUI, and TypeScript
    - Create a new React project using TypeScript
    - Add MUI for styling components
    - Implement the LifeGame component with a grid of cells
    - Add a play/pause button to control the game's execution

---
+++ src/LifeGame.tsx
@@ -0,0 +1,115 @@
+import { Box, Button, Paper } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+const resolution = 20;
+const numRows = 20;
+const numCols = 20;
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0)));
+  }
+  return rows;
+};
+
+const LifeGame: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+  const [running, setRunning] = useState(false);
+
+  const updateGrid = () => {
+    setGrid((currentGrid) => {
+      return currentGrid.map((row, y) =>
+        row.map((cell, x) => {
+          let liveNeighbors = 0;
+          for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+              if (i === 0 && j === 0) continue;
+              const y2 = (y + i + numRows) % numRows;
+              const x2 = (x + j + numCols) % numCols;
+              liveNeighbors += currentGrid[y2][x2];
+            }
+          }
+
+          if (cell === 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+            return 0;
+          } else if (cell === 0 && liveNeighbors === 3) {
+            return 1;
+          } else {
+            return cell;
+          }
+        })
+      );
+    });
+  };
+
+  useEffect(() => {
+    if (!running) {
+      return;
+    }
+    const interval = setInterval(() => {
+      updateGrid();
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running]);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        minWidth: "100vw",
+        backgroundColor: "background.default",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          display: "grid",
+          gridTemplateRows: `repeat(${numRows},
+          ${resolution}px)`,
+          gridTemplateColumns: `repeat(${numCols}, ${resolution}px)`,
+          gap: "1px",
+          backgroundColor: "grey.500",
+        }}
+      >
+        {grid.map((row, i) =>
+          row.map((col, j) => (
+            <Paper
+              key={`${i}-${j}`}
+              elevation={col ? 4 : 0}
+              sx={{
+                width: `${resolution}px`,
+                height: `${resolution}px`,
+                backgroundColor: col ? "common.black" : "common.white",
+              }}
+            />
+          ))
+        )}
+      </Paper>
+
+      <Button
+        variant="contained"
+        onClick={() => {
+          setRunning(!running);
+        }}
+        sx={{
+          position: "fixed",
+          bottom: 16,
+          right: 16,
+        }}
+      >
+        {running ? "停止" : "再生"}
+      </Button>
+    </Box>
+  );
+};
+
+export default LifeGame;
